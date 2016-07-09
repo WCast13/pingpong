053ffc7a08625e4acf7af58a1deedb07aa8efcb7
Zach Roberts <zach.ker.roberts@gmail.com>
2016-07-09 18:48:01 -0400

    Changed database to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160709125917) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "players", force: :cascade do |t|
     t.string   "name"
     t.string   "slack_name"
