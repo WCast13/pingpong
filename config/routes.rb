f1c74407600e4910173017a5bb952446e17ceab3
Zach Roberts <zach.ker.roberts@gmail.com>
2016-07-09 10:13:47 -0400

    Put button on homepage to link to create a player

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :players
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'homepage#home'
-  
+  get '/new' => 'players#new'
 end
