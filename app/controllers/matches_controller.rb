33dd74c65d71a9191d0c3946998ad6f577096d41
Zach Roberts <zach.ker.roberts@gmail.com>
2016-07-10 16:22:31 -0400

    Updated some styling info on submit score and new player pages

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -15,6 +15,7 @@ class MatchesController < ApplicationController
   # GET /matches/new
   def new
     @match = Match.new
+    
   end
 
   # GET /matches/1/edit
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,5 @@
 class Player < ApplicationRecord
-
+has_and_belongs_to_many :matches
   validates :slack_name, presence: true
   validates :slack_name, uniqueness: true
   validates :name, presence: true
