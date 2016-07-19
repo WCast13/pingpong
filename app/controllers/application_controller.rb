e54ce13ec73536b1ff464ecf519fb7693b2cc0ee
Zach Roberts <zach.ker.roberts@gmail.com>
2016-07-18 20:28:15 -0400

    admin doesn't show up in standings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   private
-  def admin_access
-    return redirect_to '/' if @current_player.user_name != "admin" || @current_player.nil?
-  end
+
   def current_player
     if session[:player_id] != nil
     @current_player = Player.find(session[:player_id])
