e3bffa208a5c9d4a3c68e16738d44134166f2cbf
Zach Roberts <zach.ker.roberts@gmail.com>
2016-07-21 10:56:32 -0400

    issues with homepage

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -4,7 +4,7 @@ class HomepageController < ApplicationController
       not_a_user
       # Player.find(2).update(standings_position: 1)
       # Player.find(1).update(standings_position: 2)
-      if @current_player
+      if @current_player && @current_player.user_name != "admin"
       @player_ladder = @current_player.standings_position
       if @current_player.standings_position != 1
       @one_up_ladder = Player.find_by(standings_position: @current_player.standings_position - 1).user_name
