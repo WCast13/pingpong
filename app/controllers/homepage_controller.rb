a3228343236e6925b5e9b263c1b9613966b95edf
Zach Roberts <zach.ker.roberts@gmail.com>
2016-07-22 11:36:54 -0400

    changed homepage to use first and last name not username

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -5,9 +5,9 @@ class HomepageController < ApplicationController
       if @current_player && @current_player.user_name != "admin"
       @player_ladder = @current_player.standings_position
       if @current_player.standings_position != 1
-      @one_up_ladder = Player.find_by(standings_position: @current_player.standings_position - 1).user_name
+      @one_up_ladder = Player.find_by(standings_position: @current_player.standings_position - 1)
       if @current_player.standings_position >= 2
-      @two_up_ladder = Player.find_by(standings_position: @current_player.standings_position - 2).user_name
+      @two_up_ladder = Player.find_by(standings_position: @current_player.standings_position - 2)
       end
     end
     end
